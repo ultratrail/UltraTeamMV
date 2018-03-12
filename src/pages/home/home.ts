34fbc5bc846f464b4a4ef5aa486608030f59ab41
leovalette <leovalette38@gmail.com>
2018-03-12 17:10:39 +0100

    first commit BLEwebapp

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {DevicePage} from '../device/device';
+import {BLE} from 'ionic-native';
+
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+isScanning : boolean;
+devices;
 
-  }
+	constructor(public nav: NavController) {
+		this.devices = [];
+		this.isScanning = false;
+	}
+
+	startScanning() {
+		console.log("Scanning Started");
+		this.devices = [];
+		this.isScanning = true;
+		BLE.startScan([]).subscribe(device => {
+			this.devices.push(device);
+		});
+
+		setTimeout(() => {
+			BLE.stopScan().then(() => {
+				console.log('Scanning has stopped');
+				console.log(JSON.stringify(this.devices))
+				this.isScanning = false;
+			});
+		}, 3000);
+
+	}
+
+	connectToDevice(device) {
+		console.log('Connect To Device');
+		console.log(JSON.stringify(device))
+		this.nav.push(DevicePage, {
+			device: device
+		});
+	}
 
 }
